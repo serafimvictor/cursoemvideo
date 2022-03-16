74f9dcf8ebb6b09e6b0b7430467405a9763a517b
serafimvictor <victor.serafim@outlook.com>
2022-03-16 01:22:52 -0300

    Exercicios aula

---
+++ cursoemvideo/python3/scripts-aulas/aula08a.py
@@ -0,0 +1,6 @@
+from math import sqrt
+
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+
+print('A raiz de {} é igual a {}.'.format(num, math.ceil(raiz)))
---
+++ cursoemvideo/python3/scripts-aulas/aula08b.py
@@ -0,0 +1,5 @@
+import random
+
+num = random.randint(1, 25)
+
+print(num)

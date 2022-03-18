6b9a2a5ae6586e307fa0357f315758c6983e0019
victorserafimnsj <victorserafim@nasajon.com.br>
2022-03-18 20:22:37 -0300

    Adicionando ate aula 9

---
+++ cursoemvideo/python3/Exercicios/ex020.py
@@ -0,0 +1,12 @@
+# O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+from random import shuffle
+
+al1 = str(input('Primeiro aluno: '))
+al2 = str(input('Segundo aluno: '))
+al3 = str(input('Terceiro aluno: '))
+al4 = str(input('Quarto aluno: '))
+lista = [al1, al2, al3, al4]
+shuffle(lista)
+
+print('A ordem de apresentação será: \n{}'.format(lista))
---
+++ cursoemvideo/python3/scripts-aulas/aula09a.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Vídeo Python'
+
+#print(frase[1:12:2])
+
+# print("""texto grande
+# grande demais
+# victor hugo
+# leitura dinamica""")

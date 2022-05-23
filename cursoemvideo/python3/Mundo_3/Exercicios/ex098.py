4f559b84a8fd7a64f54ef916ead2122f5ae2e3bb
victorserafimnsj <victorserafim@nasajon.com.br>
2022-05-23 01:34:52 -0300

    ajuste ex 98

--- cursoemvideo/python3/Mundo_3/Exercicios/ex098.py
+++ cursoemvideo/python3/Mundo_3/Exercicios/ex098.py
@@ -8,28 +8,26 @@ from time import sleep
 
 def contador(i, f, p):
     cont = 0
+    print(f'{"-" * 10} Contador de {i} a {f} pulando {p} {"-" * 10}')
+    
+    if f < i:
+        p *= -1
+        f -= 1
+    else:
+        f += 1
+    
     for c in range(i, f, p):
         cont += 1
-        print(c, end=' ')
-    sleep(1)
+        print(c, end=' ', flush = True)
+        sleep(0.5)
     print('FIM.')
 
 
-print(f'{"-" * 10} Contador de 1 a 10 pulando 1 {"-" * 10}')
-contador(1, 11, 1)
-print(f'{"-" * 10} Contador de 10 a 0 pulando 2 {"-" * 10}')
-contador(10, -1, -2)
-print(f'{"-" * 10} Agora monte sua contagem {"-" * 10}')
+contador(1, 10, 1)
+contador(10, 0, 2)
 
 inicio = int(input('Qual número você quer iniciar a contagem? '))
 fim = int(input('Qual número será o final da contagem? '))
 pulo = int(input('A contagem vai pular de quanto em quanto? '))
-
-print(f'{"-" * 10} Contador de {inicio} a {fim} pulando {pulo} {"-" * 10}')
-
-if fim < inicio:
-    pulo *= -1
-else:
-    fim += 1
 
 contador(inicio, fim, pulo)
